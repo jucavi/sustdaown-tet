f24ec176d2d6de4d705e0e06671ac8bf771131c3
Juan Carlos Vilarrubia <jcarvil@gmail.com>
2024-10-27 18:47:53 +0100

    using SpringApplication#exit

--- src/main/java/es/emi/shutdownapp/ShutdownExecutor.java
+++ src/main/java/es/emi/shutdownapp/ShutdownExecutor.java
@@ -1,13 +1,14 @@
 package es.emi.shutdownapp;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 @Log4j2
-public class ShutdownExecutor {
+public class ShutdownExecutor implements ExitCodeGenerator {
 
     private final ConfigurableApplicationContext context;
 
@@ -20,12 +21,16 @@ public class ShutdownExecutor {
         try {
             log.info("Shutting down the application...");
             //context.close();
-            int exitCode = SpringApplication.exit(context, () -> 0);
-            log.info("Application shutdown complete with exit code {}", exitCode);
+            SpringApplication.exit(context, this);
 
         } catch (Exception e) {
             log.error("Error shutting down the application: {}", e.getMessage());
             throw new RuntimeException("Application shutdown due to an error", e);
         }
+    }
+
+    @Override
+    public int getExitCode() {
+        return 0;
     }
 }
